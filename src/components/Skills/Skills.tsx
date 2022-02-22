f74154ffa3934b1df3b2696c1ff28ca1294c5035
olegweremyew <olegweremey1994@mail.ru>
2022-02-22 18:59:10 +0300

    tree part 1

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Skill} from "./Skill/Skill";
+
+export const Skills = () => {
+    return (
+        <div>
+            <Skill/>
+            <Skill/>
+        </div>
+    );
+};
