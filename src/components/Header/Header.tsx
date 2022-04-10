424747673c03abe36d0adae95d10a8d2c8be7221
olegweremyew <olegweremey1994@mail.ru>
2022-04-10 23:08:20 +0300

    header part 1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import style from "./Header.module.css"
+
+import {Nav} from "./Nav/Nav";
+import {HeaderIcon} from "./HeaderIcon/HeaderIcon";
 
 export const Header = () => {
     return (
-        <div>
-            header
+        <div className={style.header}>
+            <HeaderIcon/>
+            <Nav/>
         </div>
     );
 };
--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import style from "./Nav.module.css"
 
 export const Nav = () => {
     return (
-        <div>
-
-        </div>
+        <nav className={style.navbar}>
+            <a className={style.navbar__link} href="#">Home</a>
+            <a className={style.navbar__link} href="#">Services</a>
+            <a className={style.navbar__link} href="#">About me</a>
+            <a className={style.navbar__link} href="#">Skills</a>
+            <a className={style.navbar__link} href="#">Portfolio</a>
+            <a className={style.navbar__link} href="#">Contacts</a>
+        </nav>
     );
 };
